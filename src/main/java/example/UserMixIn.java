9df55c07796fa37388064e4e82d27f57130d6423
Peleg Tsadok <pelegtsa@gmail.com>
2022-06-18 19:51:34 +0300

    Updated MixIn

--- src/main/java/example/UserMixIn.java
+++ src/main/java/example/UserMixIn.java
@@ -1,12 +1,16 @@
 package example;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-public abstract class IgnoreSchemaProperty
+public abstract class UserMixIn
 {
     @JsonIgnore
     abstract void getSchema();
 
     @JsonIgnore
     abstract void getSpecificData();
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    abstract void getStartTime();
 }
